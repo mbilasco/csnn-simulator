ebf6554838edf24d6b6247d933e8a8118dfafcc5
Gaspard GOUPY <gaspard.goupy@etu.univ-lyon1.fr>
2022-11-28 21:29:12 +0100

    CIFAR10 task like in the paper (no)

--- apps/Cifar10Grayscale.cpp
+++ apps/Cifar10Grayscale.cpp
@@ -26,9 +26,10 @@ int main(int argc, char** argv) {
 
 	std::string input_path(input_path_ptr);
 
-	experiment.push<process::GrayScale>();
-	experiment.push<process::DefaultOnOffFilter>(7, 1.0, 2.0);
-	experiment.push<process::FeatureScaling>();
+	//experiment.push<process::GrayScale>();
+	//experiment.push<process::DefaultOnOffFilter>(7, 1.0, 2.0);
+	experiment.push<process::BiologicalOnOffFilter>(7, 1.0, 2.0);
+    experiment.push<process::FeatureScaling>();
 	experiment.push<LatencyCoding>();
 
 	experiment.add_train<dataset::Cifar>(std::vector<std::string>({
@@ -50,11 +51,11 @@ int main(int argc, char** argv) {
 	auto& conv1 = experiment.push<layer::Convolution>(5, 5, 64);
     conv1.set_name("conv1");
     conv1.parameter<uint32_t>("epoch").set(100);
-	conv1.parameter<float>("annealing").set(0.95f);
-	conv1.parameter<float>("min_th").set(4.0f);
+	//conv1.parameter<float>("annealing").set(0.95f); //default to 1
+	//conv1.parameter<float>("min_th").set(4.0f); //default to 0
 	conv1.parameter<float>("t_obj").set(t_obj);
 	conv1.parameter<float>("lr_th").set(th_lr);
-    conv1.parameter<bool>("wta_infer").set(true);
+    conv1.parameter<bool>("wta_infer").set(false); //not implemented in the public version
 	conv1.parameter<Tensor<float>>("w").distribution<distribution::Uniform>(0.0, 1.0);
 	conv1.parameter<Tensor<float>>("th").distribution<distribution::Gaussian>(20.0, 0.01);
 	conv1.parameter<STDP>("stdp").set<stdp::Multiplicative>(w_lr, 1);
@@ -62,8 +63,8 @@ int main(int argc, char** argv) {
 	auto& conv1_out = experiment.output<DefaultOutput>(conv1, 0.0, 1.0);
 	conv1_out.add_postprocessing<process::SumPooling>(2, 2);
 	conv1_out.add_postprocessing<process::FeatureScaling>();
-	conv1_out.add_analysis<analysis::Activity>();
-	conv1_out.add_analysis<analysis::Coherence>();
+	//conv1_out.add_analysis<analysis::Activity>();
+	//conv1_out.add_analysis<analysis::Coherence>();
 	conv1_out.add_analysis<analysis::Svm>();
 
 #ifdef ENABLE_QT
