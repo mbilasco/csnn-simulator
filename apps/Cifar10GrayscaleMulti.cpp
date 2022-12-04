5662105ad88b95d903d5277e44981194e954a9a6
Gaspard GOUPY <gaspard.goupy@etu.univ-lyon1.fr>
2022-12-04 09:30:21 +0100

    New best params cifar10 multi layer

--- apps/Cifar10GrayscaleMulti.cpp
+++ apps/Cifar10GrayscaleMulti.cpp
@@ -44,21 +44,17 @@ int main(int argc, char** argv) {
 		input_path+"test_batch.bin"
 	}));
 
-	float t_obj = 0.95f;
-	float th_lr = 1.0f;
-	float w_lr = 0.1f;
-
 	auto& conv1 = experiment.push<layer::Convolution>(5, 5, 64);
 	conv1.set_name("conv1");
 	conv1.parameter<uint32_t>("epoch").set(100);
-	conv1.parameter<float>("annealing").set(0.95f); //not specified in the paper Pattern Recognition
-	conv1.parameter<float>("min_th").set(4.0f); //not specified in the paper Pattern Recognition
-	conv1.parameter<float>("t_obj").set(t_obj);
-	conv1.parameter<float>("lr_th").set(th_lr);
+	conv1.parameter<float>("annealing").set(0.99); //not specified in the paper Pattern Recognition
+	conv1.parameter<float>("min_th").set(2.0); //not specified in the paper Pattern Recognition
+	conv1.parameter<float>("t_obj").set(0.9);
+	conv1.parameter<float>("lr_th").set(1.0);
 	conv1.parameter<bool>("wta_infer").set(false); //not implemented in the public version + not specified in the paper Pattern Recognition
 	conv1.parameter<Tensor<float>>("w").distribution<distribution::Uniform>(0.0, 1.0);
-	conv1.parameter<Tensor<float>>("th").distribution<distribution::Gaussian>(10.0, 0.1); //not as in the paper Pattern Recognition
-	conv1.parameter<STDP>("stdp").set<stdp::Multiplicative>(w_lr, 1);
+	conv1.parameter<Tensor<float>>("th").distribution<distribution::Gaussian>(6.0, 0.1); //not as in the paper Pattern Recognition
+	conv1.parameter<STDP>("stdp").set<stdp::Multiplicative>(0.1, 1);
 
 	auto& pool1 = experiment.push<layer::Pooling>(2, 2, 2, 2);
 	pool1.set_name("pool1");
@@ -66,45 +62,46 @@ int main(int argc, char** argv) {
 	auto& conv2 = experiment.push<layer::Convolution>(5, 5, 128);
 	conv2.set_name("conv2");
 	conv2.parameter<uint32_t>("epoch").set(100);
-	conv2.parameter<float>("annealing").set(0.95f);
-	conv2.parameter<float>("min_th").set(6.0f);
-	conv2.parameter<float>("t_obj").set(t_obj);
-	conv2.parameter<float>("lr_th").set(th_lr);
+	conv2.parameter<float>("annealing").set(0.99);
+	conv2.parameter<float>("min_th").set(4.0f);
+	conv2.parameter<float>("t_obj").set(0.9);
+	conv2.parameter<float>("lr_th").set(1.0);
 	conv2.parameter<bool>("wta_infer").set(false);
 	conv2.parameter<Tensor<float>>("w").distribution<distribution::Uniform>(0.0, 1.0);
-	conv2.parameter<Tensor<float>>("th").distribution<distribution::Gaussian>(14.0, 0.1);
-	conv2.parameter<STDP>("stdp").set<stdp::Multiplicative>(w_lr, 1);
+	conv2.parameter<Tensor<float>>("th").distribution<distribution::Gaussian>(10.0, 0.1);
+	conv2.parameter<STDP>("stdp").set<stdp::Multiplicative>(0.1, 1);
 
 	auto& pool2 = experiment.push<layer::Pooling>(2, 2, 2, 2);
 	pool2.set_name("pool2");
 
+	// Activity after conv1
 	auto& conv1_anal = experiment.output<DefaultOutput>(conv1, 0.0, 1.0);
 	conv1_anal.add_analysis<analysis::Activity>();
 
+	// SVM accuracy after conv1
 	auto& conv1_out = experiment.output<DefaultOutput>(conv1, 0.0, 1.0);
 	conv1_out.add_postprocessing<process::SumPooling>(2, 2);
 	conv1_out.add_postprocessing<process::FeatureScaling>();
 	conv1_out.add_analysis<analysis::Svm>();
 
+	// Activity after conv2
 	auto& conv2_anal = experiment.output<DefaultOutput>(conv2, 0.0, 1.0);
 	conv2_anal.add_analysis<analysis::Activity>();
 
+	// SVM accuracy after conv2
 	auto& conv2_out = experiment.output<DefaultOutput>(conv2, 0.0, 1.0);
 	conv2_out.add_postprocessing<process::SumPooling>(2, 2);
 	conv2_out.add_postprocessing<process::FeatureScaling>();
 	conv2_out.add_analysis<analysis::Svm>();
 
+	// Save pool2 features
 	auto& pool2_save = experiment.output<NoOutputConversion>(pool2);
 	pool2_save.add_analysis<analysis::SaveOutputJson>("pool2_train.json", "pool2_test.json");
 
+	// SVM Accuracy after pool2 (no sum pooling)
 	auto& pool2_out = experiment.output<DefaultOutput>(pool2, 0.0, 1.0);
 	pool2_out.add_analysis<analysis::Activity>();
 	pool2_out.add_analysis<analysis::Svm>();
-
-#ifdef ENABLE_QT
-	conv1.plot_threshold(true);
-	conv1.plot_reconstruction<process::GaussianTemporalCodingColor>(true);
-#endif
 
 	experiment.run(10000);
 }
