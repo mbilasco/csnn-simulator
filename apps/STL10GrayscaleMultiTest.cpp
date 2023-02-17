7fae8f4f90f808dabc1b044e50707576aaf9780b
Gaspard GOUPY <gaspard.goupy@etu.univ-lyon1.fr>
2023-02-17 09:09:24 +0100

    Test stl

--- apps/STL10GrayscaleMultiTest.cpp
+++ apps/STL10GrayscaleMultiTest.cpp
@@ -18,7 +18,7 @@
 
 int main(int argc, char** argv) {
 
-	Experiment<DenseIntermediateExecution> experiment(argc, argv, "stl_10gs_multi");
+	Experiment<DenseIntermediateExecution> experiment(argc, argv, "stl_10gs_multi_test");
 
 	const char* input_path_ptr = std::getenv("INPUT_PATH");
 
@@ -36,7 +36,7 @@ int main(int argc, char** argv) {
 	experiment.add_train<dataset::STL>(input_path+"train_X.bin", input_path+"train_y.bin");
 	experiment.add_test<dataset::STL>(input_path+"test_X.bin", input_path+"test_y.bin");
 
-	auto& conv1 = experiment.push<layer::Convolution>(5, 5, 64);
+	auto& conv1 = experiment.push<layer::Convolution>(7, 7, 64);
 	conv1.set_name("conv1");
 	conv1.parameter<uint32_t>("epoch").set(200);
 	conv1.parameter<float>("annealing").set(0.99);
@@ -45,13 +45,13 @@ int main(int argc, char** argv) {
 	conv1.parameter<float>("lr_th").set(0.1);
 	conv1.parameter<bool>("wta_infer").set(false);
 	conv1.parameter<Tensor<float>>("w").distribution<distribution::Uniform>(0.0, 1.0);
-	conv1.parameter<Tensor<float>>("th").distribution<distribution::Gaussian>(10.0, 0.1);
+	conv1.parameter<Tensor<float>>("th").distribution<distribution::Gaussian>(15.0, 0.1);
 	conv1.parameter<STDP>("stdp").set<stdp::Multiplicative>(0.1, 1);
 
 	auto& pool1 = experiment.push<layer::Pooling>(4, 4, 4, 4);
 	pool1.set_name("pool1");
 
-	auto& conv2 = experiment.push<layer::Convolution>(3, 3, 128);
+	auto& conv2 = experiment.push<layer::Convolution>(5, 5, 128);
 	conv2.set_name("conv2");
 	conv2.parameter<uint32_t>("epoch").set(200);
 	conv2.parameter<float>("annealing").set(0.99);
@@ -86,8 +86,8 @@ int main(int argc, char** argv) {
 	pool1_out.add_analysis<analysis::Svm>();
 
 	// pool2 : Save features
-	auto& pool2_save = experiment.output<SpikeTiming>(pool2);
-	pool2_save.add_analysis<analysis::SaveOutputNumpy>("pool2_4x4");
+	//auto& pool2_save = experiment.output<SpikeTiming>(pool2);
+	//pool2_save.add_analysis<analysis::SaveOutputNumpy>("pool2_4x4");
 
 	// pool2 : Activity
 	auto& pool2_activity = experiment.output<DefaultOutput>(pool2, 0.0, 1.0);
