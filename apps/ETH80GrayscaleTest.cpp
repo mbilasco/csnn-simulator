81d165afd744c7b01dabd7903d1a6d54bd279dfa
Gaspard Goupy <gaspard.goupy@univ-lille.fr>
2023-04-05 16:30:42 +0200

    Reset test dataset

--- apps/ETH80GrayscaleTest.cpp
+++ apps/ETH80GrayscaleTest.cpp
@@ -28,8 +28,8 @@ int main(int argc, char** argv) {
 
 	std::string input_path(input_path_ptr);
 
-	//experiment.push<process::GrayScale>();
-	//experiment.push<process::DefaultOnOffFilter>(7, 1.0, 2.0);
+	experiment.push<process::GrayScale>();
+	experiment.push<process::DefaultOnOffFilter>(7, 1.0, 2.0);
 	//experiment.push<process::FeatureScaling>();
 	experiment.push<LatencyCoding>();
 
@@ -47,7 +47,7 @@ int main(int argc, char** argv) {
 	conv1.parameter<bool>("wta_infer").set(false);
 	//conv1.parameter<Tensor<float>>("w").distribution<distribution::Uniform>(0.0, 1.0);
 	conv1.parameter<Tensor<float>>("w").distribution<distribution::Gaussian>(0.5, 0.01);
-	conv1.parameter<Tensor<float>>("th").distribution<distribution::Constant>(10.0);
+	conv1.parameter<Tensor<float>>("th").distribution<distribution::Constant>(6.0);
 	conv1.parameter<STDP>("stdp").set<stdp::Multiplicative>(0.1, 0);
 	//conv1.parameter<STDP>("stdp").set<stdp::Biological>(0.1, 0.1f);
 
