defb5bc6e4830c717fc6b9c39729a327ec3fa141
Marius BILASCO <marius.bilasco@univ-lille.fr>
2024-12-16 12:41:00 +0100

    Adding MaxPooling for 4D featuremaps

--- include/process/Pooling.h
+++ include/process/Pooling.h
@@ -144,6 +144,27 @@ namespace process
 		size_t _height;
 		size_t _depth;
 	};
+
+	class MaxPooling : public UniquePassProcess {
+
+	public:
+		MaxPooling();
+		MaxPooling(size_t target_width, size_t target_height);
+
+		virtual Shape compute_shape(const Shape& shape);
+		virtual void process_train(const std::string& label, Tensor<float>& sample);
+		virtual void process_test(const std::string& label, Tensor<float>& sample);
+
+	private:
+		void _process(Tensor<float>& sample) const;
+
+		size_t _target_width;
+		size_t _target_height;
+
+		size_t _width;
+		size_t _height;
+		size_t _depth;
+	};
 }
 
 #endif
